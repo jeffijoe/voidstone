9ee7ec798cf1f8357ac3460520c0e41c33fb600a
Jeff Hansen <jeffijoe@hotmail.com>
2016-02-18 14:01:58 +0100

    Visibility stuff, nice.

--- Voidstone/MainWindow.xaml.cs
+++ Voidstone/MainWindow.xaml.cs
@@ -7,21 +7,57 @@
 
 using System.Windows;
 
+using Jeffijoe.Voidstone.ViewModels;
+
+using ReactiveUI;
+
 namespace Jeffijoe.Voidstone
 {
     /// <summary>
     ///     Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IViewFor<MainViewModel>
     {
         #region Constructors and Destructors
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        ///     Initializes a new instance of the <see cref="MainWindow" /> class.
         /// </summary>
         public MainWindow()
         {
             this.InitializeComponent();
+            this.DataContext = this.ViewModel = new MainViewModel();
+            this.WhenAnyObservable(x => x.ViewModel.Start.IsExecuting)
+                        .BindTo(this, x => x.WorkingOnItLabel.Visibility);
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets the view model.
+        /// </summary>
+        public MainViewModel ViewModel { get; set; }
+
+        #endregion
+
+        #region Explicit Interface Properties
+
+        /// <summary>
+        /// Gets or sets the view model.
+        /// </summary>
+        object IViewFor.ViewModel
+        {
+            get
+            {
+                return this.ViewModel;
+            }
+
+            set
+            {
+                this.ViewModel = (MainViewModel)value;
+            }
         }
 
         #endregion
--- Voidstone/ViewModels/MainViewModel.cs
+++ Voidstone/ViewModels/MainViewModel.cs
@@ -5,15 +5,144 @@
 // Author: Jeff Hansen <jeff@jeffijoe.com>
 // Copyright (C) Jeff Hansen 2016. All rights reserved.
 
+using System;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
 using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
 
 namespace Jeffijoe.Voidstone.ViewModels
 {
     /// <summary>
-    /// Main view model.
+    ///     Main view model.
     /// </summary>
     public class MainViewModel : ReactiveObject
     {
-        
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainViewModel"/> class.
+        /// </summary>
+        public MainViewModel()
+        {
+            this.FramerateString = "24";
+            var obs = this.WhenAny(
+                x => x.FramerateString,
+                x => {
+                    int number;
+                    if (int.TryParse(x.Value, out number))
+                    {
+                        return number;
+                    }
+
+                    return 0;
+                });
+
+            obs.ToPropertyEx(this, x => x.Framerate);
+            obs
+                .Select(x => x.ToString())
+                .Where(x => x != this.FramerateString)
+                .ObserveOn(Application.Current.Dispatcher)
+                .Subscribe(
+                    x => {
+                        this.FramerateString = x;
+                    });
+
+            this.CanStart = this.WhenAny(
+                x => x.Framerate,
+                x => x.InputDirectory,
+                x => x.InputFilePattern,
+                x => x.OutputFile,
+                (fr, id, ifp, of) => {
+                    if (fr.Value == 0) return false;
+                    if (string.IsNullOrWhiteSpace(id.Value)) return false;
+                    if (string.IsNullOrWhiteSpace(ifp.Value)) return false;
+                    if (string.IsNullOrWhiteSpace(of.Value)) return false;
+                    return true;
+                });
+
+            this.Start = ReactiveCommand.CreateAsyncTask(
+                this.CanStart,
+                async x => {
+                    Console.WriteLine("Starting! :D");
+                    await Task.Delay(3000);
+                    Console.WriteLine("Done! :D");
+                });
+        }
+
+        /// <summary>
+        /// Gets the can start.
+        /// </summary>
+        /// <value>
+        /// The can start.
+        /// </value>
+        public IObservable<bool> CanStart { get; private set; }
+
+        /// <summary>
+        /// Gets the is busy.
+        /// </summary>
+        /// <value>
+        /// The is busy.
+        /// </value>
+        public IObservable<bool> IsBusy { get; private set; }
+
+        #region Public Properties
+
+
+        /// <summary>
+        /// Gets the framerate.
+        /// </summary>
+        /// <value>
+        /// The framerate.
+        /// </value>
+        [ObservableAsProperty]
+        public extern int Framerate { get; }
+
+        /// <summary>
+        /// Gets or sets the framerate string.
+        /// </summary>
+        /// <value>
+        /// The framerate string.
+        /// </value>
+        [Reactive]
+        public string FramerateString { get; set; }
+
+        /// <summary>
+        /// Gets or sets the input directory.
+        /// </summary>
+        /// <value>
+        /// The input directory.
+        /// </value>
+        [Reactive]
+        public string InputDirectory { get; set; }
+
+        /// <summary>
+        /// Gets or sets the input file pattern.
+        /// </summary>
+        /// <value>
+        /// The input file pattern.
+        /// </value>
+        [Reactive]
+        public string InputFilePattern { get; set; }
+
+        /// <summary>
+        /// Gets or sets the output file.
+        /// </summary>
+        /// <value>
+        /// The output file.
+        /// </value>
+        [Reactive]
+        public string OutputFile { get; set; }
+
+        /// <summary>
+        /// Gets the start.
+        /// </summary>
+        /// <value>
+        /// The start.
+        /// </value>
+        public ReactiveCommand<Unit> Start { get; private set; }
+
+        #endregion
     }
 }
