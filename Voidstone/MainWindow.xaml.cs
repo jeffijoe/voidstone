071797ec53c46784858cf4be788d78c3f0063f74
Jeff Hansen <jeffijoe@hotmail.com>
2016-02-18 12:28:08 +0100

    Initial commit.

---
+++ Voidstone/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿// Voidstone
+// - Voidstone
+// -- MainWindow.xaml.cs
+// -------------------------------------------
+// Author: Jeff Hansen <jeff@jeffijoe.com>
+// Copyright (C) Jeff Hansen 2016. All rights reserved.
+
+using System.Windows;
+
+namespace Jeffijoe.Voidstone
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Constructors and Destructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+    }
+}
---
+++ Voidstone/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿// Voidstone
+// - Voidstone
+// -- MainViewModel.cs
+// -------------------------------------------
+// Author: Jeff Hansen <jeff@jeffijoe.com>
+// Copyright (C) Jeff Hansen 2016. All rights reserved.
+
+using ReactiveUI;
+
+namespace Jeffijoe.Voidstone.ViewModels
+{
+    /// <summary>
+    /// Main view model.
+    /// </summary>
+    public class MainViewModel : ReactiveObject
+    {
+        
+    }
+}
